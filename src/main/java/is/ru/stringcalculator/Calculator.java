3693dfa9cbe3c85c467fc81b2b5b4d6549f9bd5b
Dagur Arinbjörn Daníelsson <dagur13@i-ee251479.greenqloud.internal>
2014-10-17 14:24:49 +0000

    kata 4 has been pwnÃ-ed

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,12 +3,20 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
+		String delimeter = ",";
+		//are we adding a new delimeter
+		if(text.startsWith("//")){
+			// set delimeter to the third char in string
+			delimeter = String.valueOf(text.charAt(2));
+			text = text.substring(text.indexOf('\n')+1);
+		}
+		
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")
+		else if(text.contains(delimeter)
 			|| text.contains("\n")){
-			return sum(splitNumbers(text));
+			return sum(splitNumbers(text, delimeter));
 		}
 		else
 			return 1;
@@ -18,18 +26,15 @@ public class Calculator {
 		return Integer.parseInt(number);
 	}
 
-	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",|\\n");
+	private static String[] splitNumbers(String numbers, String delimeter){
+	    return numbers.split(delimeter + "|\\n");
 	}
       
     private static int sum(String[] numbers){
- 	    int total = 0;
+	int total = 0;
         for(String number : numbers){
 		    total += toInt(number);
 		}
 		return total;
     }
-
-
-
 }
